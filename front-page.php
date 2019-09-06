a27ef4b4a25b890ece5d012120c1327270c6203c
Ben Kaminski <ben@benkaminski.com>
2019-09-06 19:54:29 -0400

    basic landing page added

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
+<div class="container mt-5">
+	<img class="img-fluid mx-auto d-block pt-5" src="<?php echo get_template_directory_uri(); ?>/assets/img/integrity-sound-solutions-logo2.png" alt="Integrity Sound Solutions" />
 
-
-
-
-
+	<h1 class="text-center mt-5">Integrity Sound Solutions</h1>
+</div>
 <?php get_footer(); ?>
