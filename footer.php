7f620e3ef2d3e52c036ac35bd97856699d834939
Ben Kaminski <ben@benkaminski.com>
2019-09-07 14:03:02 -0400

    added contact form 7 and styles

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
+<footer class="container">
+	<address class="text-center">
+		<strong>Integrity Sound Solutions</strong>
+		<br />
+		<p>3395 S. DuPont Hwy<br />
+		Camden, DE 19934<br />
+	    (302) 990-2776</p>
+	</address>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
