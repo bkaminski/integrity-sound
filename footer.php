b5a4797fa9bb8a00677070111082676f60fe0123
Ben Kaminski <ben@benkaminski.com>
2019-10-29 16:25:55 -0400

    home page sections intact, colors coming

--- footer.php
+++ footer.php
@@ -1,12 +1,19 @@
-<footer class="container">
-	<address class="text-center">
-		<strong>Integrity Sound Solutions</strong>
-		<br />
-		<p>3395 S. DuPont Hwy<br />
-		Camden, DE 19934<br />
-	    (302) 990-2776</p>
-	</address>
+<footer class="footer text-center">
+	<div class="container-fluid pb-2 pt-4">
+		<address class="text-center text-white">
+			<strong>Integrity Sound Solutions</strong>
+			<br />
+			<p>3395 S. DuPont Hwy<br />
+			Camden, DE 19934<br />
+		    (302) 990-2776</p>
+		</address>
+	</div>
 </footer>
+<div class="sub-footer">
+		<div class="container pt-3 pb-2">
+			<p class="text-center text-light small">Copyright &copy; <?php echo date('Y'); ?> Integrity Sound Solutions, LLC</p>
+		</div>
+	</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- parts/home-about.php
+++ parts/home-about.php
@@ -2,5 +2,6 @@
 	<div class="container text-center pt-5">
 		<h1 class="pb-4">Integrity Sound Solutions</h1>
 		<p>Morbi consequat pretium mauris sit amet rhoncus. Praesent massa sem, euismod sit amet maximus et, feugiat condimentum risus. Sed gravida a diam id volutpat. Proin felis nunc, scelerisque id arcu et, convallis rutrum nunc.</p>
+		<button class="btn btn-outline-light btn-lg rounded-0">More about us</button>
 	</div>
 </main>
